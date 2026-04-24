7c49e45bccffc3cb28e38a4ba35a941645f281a2
Jim Love <james@id-live.com>
2026-04-24 15:55:35 +0100

    Simplified props

--- src/panoramsp/models/IBaseViewProps.ts
+++ src/panoramsp/models/IBaseViewProps.ts
@@ -1,11 +1,9 @@
 import type { BaseComponentContext } from "@microsoft/sp-component-base";
 import type { ISearchResult } from "@pnp/sp/search";
-import { ICustomTemplateMapping } from "./ICustomTemplateMapping";
 
-export type IViewData = ISearchResult;
+export type IViewData = ISearchResult[];
 
 export interface IBaseViewProps {
   data: IViewData;
-  templateMappings: ICustomTemplateMapping[];
-  context: BaseComponentContext;
+  context?: BaseComponentContext;
 }
