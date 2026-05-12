452bae4ecfeb12d2351a26e3e0af3ec2774f02dc
Jim Love <james@id-live.com>
2026-05-12 10:02:34 +0100

    Added sorting callback

--- src/panoramsp/models/IBaseViewProps.ts
+++ src/panoramsp/models/IBaseViewProps.ts
@@ -1,5 +1,5 @@
 import type { BaseComponentContext, IReadonlyTheme } from "@microsoft/sp-component-base";
-import type { ISearchResult } from "@pnp/sp/search";
+import type { ISearchResult, SortDirection } from "@pnp/sp/search";
 
 /**
  * Data that is passed to the view to render. This is usually a collection of items.
@@ -35,4 +35,14 @@ export interface IBaseViewProps {
    * The theme of the web part
    */
   theme?: IReadonlyTheme;
+
+  /**
+   * Callback function to handle sorting of items.
+   * This callback is passed back into PanoramSP to handle sorting of all items, and the view is responsible for 
+   * calling this callback with the appropriate property and direction when a sort action is triggered by the user.
+   * @param property The property to sort by
+   * @param direction The direction to sort (ascending or descending)
+   * @returns void
+   */
+  onSort?: (property: string, direction: SortDirection) => void;
 }
